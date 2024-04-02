9f8edb182bdd34e7850014a7bc3cc3f4762567ea
locvt1 <locvt1@hdbank.com.vn>
2024-04-02 15:51:50 +0700

    update add DB connection pool

--- config/database_configuration.go
+++ config/database_configuration.go
@@ -7,6 +7,7 @@ import (
 	"gorm.io/gorm/logger"
 	"log"
 	"os"
+	"time"
 )
 
 func InitDatabaseConnection() (db *gorm.DB, err error) {
@@ -54,7 +55,19 @@ func InitDatabaseConnection() (db *gorm.DB, err error) {
 	})
 	if err != nil {
 		log.Fatalln(err)
+		return db, err
 	}
+
+	sqlDB, err := db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	// migrateErr := db.AutoMigrate(&model.User{})
 	// if migrateErr != nil {
